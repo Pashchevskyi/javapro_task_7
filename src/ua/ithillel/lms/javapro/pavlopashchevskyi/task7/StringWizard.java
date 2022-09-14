79d25eadf297cf54e4aa23bfa154a0c73f6322a8
Pavlo Pashchevskyi <googalltooth@gmail.com>
2022-09-14 22:53:30 +0300

    null values processing in StringWizard methods. Refactoring of stringReverse() method of StringWizard

--- src/ua/ithillel/lms/javapro/pavlopashchevskyi/task7/StringWizard.java
+++ src/ua/ithillel/lms/javapro/pavlopashchevskyi/task7/StringWizard.java
@@ -2,6 +2,9 @@ package ua.ithillel.lms.javapro.pavlopashchevskyi.task7;
 
 public class StringWizard {
     public int findSymbolOccurance(String str, char ch) {
+        if (str == null) {
+            return 0;
+        }
         int result = -1;
         int times = 0;
         do {
@@ -15,17 +18,30 @@ public class StringWizard {
     }
 
     public int findWordPosition(String source, String target) {
+        if (source == null || target == null) {
+            return -1;
+        }
         return source.indexOf(target);
     }
 
     public String stringReverse(String str) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(str);
+        if (str == null) {
+            System.out.println("ERROR: unable to reverse null instead of string");
+            return "";
+        }
+        int n = str.length();
+        char strInversedChars[] = new char[n];
+        for (int j = 0; j < n; j++) {
+            strInversedChars[j] = str.charAt(n - j - 1);
+        }
 
-        return sb.reverse().toString();
+        return new String(strInversedChars);
     }
 
     public boolean isPalindrome(String str) {
+        if (str == null) {
+            return false;
+        }
         int j = str.length() - 1;
         char strChars[] = str.toCharArray();
         for (int i = 0; i < str.length(); i++) {
